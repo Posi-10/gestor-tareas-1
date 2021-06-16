8a2ded112c595565bebb12aa27876bbf89020577
Fernando Vasquez <53883345+Swumplurd@users.noreply.github.com>
2021-06-16 08:00:05 -0500

    carpetas y configuracion listas

---
+++ app/dependencias.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="<?=SERVIDOR?>lib/bootstrap/css/bootstrap.min.css">
+
+<script src="<?=SERVIDOR?>lib/jquery/jquery3.6.0.js"></script>
+<script src="<?=SERVIDOR?>lib/popper/popper.js"></script>
+<script src="<?=SERVIDOR?>lib/bootstrap/js/bootstrap.min.js"></script>
